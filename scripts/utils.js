13724e51ef10f3d610fe1d1b60b026594ed3b8ad
Lucas Ferreira Soares <105013659+LucasTrawman@users.noreply.github.com>
2022-12-12 22:05:46 -0300

    login api arrumado

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,12 @@
-/* ADICIONADO DO PROF -> Retira espaços iniciais e finais de uma string informada */
 function normalizaStringUsandoTrim(textoRecebido) {
   return textoRecebido.trim();
 }
 
-/* ADICIONADO DO PROF -> BASE-URL da API que o projeto realiza a conexão */
 function apiBaseURL() {
-  return "http://todo-api.ctd.academy:3000/v1"
+  return "http://todo-api.ctd.academy:3000/v1";
 }
 
-// Sessão de LOGIN
+
 
 let formulario = document.querySelector("form");
 let btnAcessar = document.querySelector("#btn_acessar");
@@ -17,19 +15,15 @@ let emailErro = document.querySelector("#EmailErro");
 let inputSenha = document.querySelector("#inputPassword");
 let senhaErro = document.querySelector("#SenhaErro");
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-// Define um objeto JS para o usuário
+let loginUsuarioJson = "";
 let loginUsuario = {
   email: "",
-  password: ""
-}
+  password: "",
+};
 
-// Define um objeto JSON para o usuário
-let loginUsuarioJson = "";
 
-// Validar campo email vazio/nulo
+
 function validarEmail() {
-
   if (emailRegex.test(inputEmail.value)) {
     inputEmail.classList.add("campo_correto");
     emailErro.classList.add("hidden");
@@ -40,7 +34,8 @@ function validarEmail() {
   }
 }
 
-// Validar campo senha vazio/nulo
+
+
 function validarSenha() {
   if (!inputSenha.value) {
     inputSenha.classList.add("campo_incorreto");
@@ -55,17 +50,20 @@ function validarSenha() {
 }
 
 
-// Validar botão
+
 function validarAcessar() {
-   if(emailRegex.test(inputEmail.value) && inputSenha.value.length > 0){
+  if (emailRegex.test(inputEmail.value) && inputSenha.value.length > 0) {
     btnAcessar.classList.remove("disabled");
-   }else{
+    return true;
+  } else {
     btnAcessar.classList.add("disabled");
-   }
+    return false;
+  }
 }
 
-// Validar campos de e-mail e senha
-formulario.addEventListener("keyup", function() {
+
+
+formulario.addEventListener("keyup", function () {
   validarEmail();
   inputEmail.value.trim();
   validarAcessar();
@@ -76,80 +74,66 @@ formulario.addEventListener("keyup", function() {
 
 
 
-/* ADICIONADO DO PROF -> Adiciona um evento de click ao botão de acessar */
-btnAcessar.addEventListener("click", function (evento) {
+btnAcessar.addEventListener("click", async function (evento) {
+  emailLogin = document.querySelector("#inputEmail");
+  passwordLogin = document.querySelector("#inputPassword");
 
-  if (validaLogin()) {
+  if (validarAcessar()) {
+    evento.preventDefault();
 
-      evento.preventDefault();
+    emailLogin = normalizaStringUsandoTrim(emailLogin.value);
+    passwordLogin = normalizaStringUsandoTrim(passwordLogin.value);
 
-      /* Normalizando os inputs da tela de login */
-      inputEmail = normalizaStringUsandoTrim(inputEmail.value);
-      inputSenha = normalizaStringUsandoTrim(inputSenha.value);
+    loginUsuario.email = emailLogin;
+    loginUsuario.password = passwordLogin;
 
-      //Atribui as informações normalizadas ao objeto JS
-      loginUsuario.email = inputEmail;
-      loginUsuario.password = inputSenha;
+    loginUsuarioJson = JSON.stringify(loginUsuario);
 
-      //Converte o objeto JS para JSON
-      loginUsuarioJson = JSON.stringify(loginUsuario);
-
-      /* Chama a função que conecta com a API */
-      loginApi(loginUsuarioJson);
-
+    loginApi(loginUsuarioJson);
   } else {
-      console.log("Login inválido");
+    console.log("Login inválido");
   }
-
 });
 
-/* ADICIONADO DO PROF -> Comunicando com a API utilizando Fetch e Promisses */
-function loginApi(objetoUsuarioJson) {
 
+
+function loginApi(objetoUsuarioJson) {
+  
   let configRequest = {
-      method: "POST",
-      body: objetoUsuarioJson,
-      headers: {
-          'Content-type': 'application/json'
-      }
-  }
+    method: "POST",
+    body: objetoUsuarioJson,
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
 
   fetch(`${apiBaseURL()}/users/login`, configRequest)
-      .then(resultado => {
-
-          /* Verifica status de sucesso na execução da promisse */
-          if (resultado.status == 201 || resultado.status == 200) {
-              return resultado.json();
-          } else {
-              /* Caso o status não seja sucesso, retorna uma exceção com todo o objeto do "erro" */
-              throw resultado;
-          }
+    .then((resultado) => {
+      if (resultado.status == 201 || resultado.status == 200) {
+        return resultado.json();
+      } else {
+        throw resultado;
       }
-      ).then(
-          resultado => {
-              //Chama função ao obter sucesso no login
-              loginSucesso(resultado);
-          }
-      ).catch(
-          erro => {
-              //Chama função ao obter algum erro
-              loginErro(erro);
-          }
-      );
+    })
+    .then((resultado) => {
+      loginSucesso(resultado);
+    })
+    .catch((erro) => {
+      loginErro(erro);
+    });
 }
+
 
 
 function loginSucesso(respostaApi) {
-  console.log(respostaApi);
-  //Salvando o token do usuário na sessão do navegador (Usando Storage)
-  sessionStorage.setItem("jwt", respostaApi.jwt)
-  // Direcionar o usuário para a tela principal
+  sessionStorage.setItem("jwt", respostaApi.jwt);
   window.location.href = "tarefas.html";
 }
 
+
+
 function loginErro(respostaApi) {
   if (respostaApi.status == 400 || respostaApi.status == 404) {
-      console.log("Email e/ou senha inválidos");
-      alert("Email e/ou senha inválidos");
+    alert("Email e/ou senha inválidos");
   }
 }
